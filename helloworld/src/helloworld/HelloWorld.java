841167ab713d93876ab72a23095b834b8027aec1
DJRoche509 <davjroche@gmail.com>
2022-04-14 17:45:37 -0400

    First time creating Hello world

--- helloworld/src/helloworld/HelloWorld.java
+++ helloworld/src/helloworld/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello Wolrd !!!" );
+		System.out.print("Hello, Wolrd !!!" );
 
 	}
 
